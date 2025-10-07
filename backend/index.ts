70105a850f5c58685171e7574ed2a74ac2fd5cca
Guilherme Dantas <guilhermedantasgoes@gmail.com>
2025-10-06 23:54:59 -0300

    fix backend

--- backend/index.ts
+++ backend/index.ts
@@ -62,13 +62,7 @@ fastify.get("/auth/github/callback", async function (req, reply) {
       path: "/",
       sameSite: "lax",
     });
-
-    reply.send({
-      code: 200,
-      message: "Authentication successful",
-      access_token: access_token,
-    })
-
+    
     // Redireciona para o frontend
     return reply.redirect("https://api-docs-gen.vercel.app/repo");
   } catch (err) {
